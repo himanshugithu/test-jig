0e9a0c3192cc69a6a395ab5a894ac7400c8bfbc2
himanshugithu <fanibharehimanshu@gmail.com>
2024-07-15 12:31:18 +0530

    add DS18B20

---
+++ lib/GPIO/DS18B20.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import time
+
+# Initialize the 1-Wire interface
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+# Define the base directory and device folder
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    with open(device_file, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos + 2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
+while True:
+    print("Temperature: {:.2f}°C".format(read_temp()))
+    time.sleep(1)
